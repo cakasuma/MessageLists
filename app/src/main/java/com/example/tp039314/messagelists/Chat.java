19e0f4db9b51ad51b267ca4c3a8ee5909e88ecd3
MUSTOFA GHALEB AMAMI <TP039314@mail.apu.edu.my>
2017-06-19 15:36:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tp039314/messagelists/Chat.java
@@ -0,0 +1,20 @@
+
+
+/**
+ * Created by tp039314 on 19/6/2017.
+ */
+import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Chat extends Fragment{
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.chat, container, false);
+
+        return rootView;
+    }
+    }
---
+++ app/src/main/java/com/example/tp039314/messagelists/Contacts.java
@@ -0,0 +1,20 @@
+package com.example.tp039314.messagelists;
+
+/**
+ * Created by tp039314 on 19/6/2017.
+ */
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Contacts extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.contacts, container, false);
+
+        return rootView;
+    }
+}
